6dac5bd5b38e15da5be8aeee4060f9aaaf573f1c
p.olifer <pavel.olifer@gmail.com>
2020-02-28 05:16:51 +0300

    small fixes in models

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -8,7 +8,7 @@ class Customer(models.Model):
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-    def __str(self):
+    def __str__(self):
         return self.name
 
 
@@ -28,7 +28,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
 
@@ -43,7 +43,7 @@ class Order(models.Model):
         ('Delivered', 'Delivered')
     )
 
-    #customer
-    #product
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status =  models.CharField(max_length=200, null=True, choices=STATUS)
