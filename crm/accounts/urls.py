b4788363c4281a360cba58be2fca5ba2f9820db5
polly <polly@Pavels-MacBook-Air.local>
2020-03-02 12:20:43 +0300

    add login/register forms

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 
 urlpatterns = [
+    path('register/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login'),
+
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
     path('customer/<str:id>/', views.customer, name='customer'),
+
     path('create_order/<str:customer_id>/', views.create_order, name='create_order'),
     path('update_order/<str:id>/', views.update_order, name='update_order'),
     path('delete_order/<str:id>/', views.delete_order, name='delete_order'),
--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 from .models import *
 from .forms import OrderForm
 from .filters import OrderFilter
+
+
+def register_page(request):
+    form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+def login_page(request):
+    context = {
+
+    }
+    return render(request, 'accounts/login.html', context)
 
 
 def home(request):
