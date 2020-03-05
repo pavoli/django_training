f086ef7753152edac4fbc03858be855b09723fb8
polly <polly@Pavels-MacBook-Air.local>
2020-03-05 11:12:45 +0300

    add reset password process, smtp settings

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 
@@ -17,4 +19,21 @@ urlpatterns = [
     path('create_order/<str:customer_id>/', views.create_order, name='create_order'),
     path('update_order/<str:id>/', views.update_order, name='update_order'),
     path('delete_order/<str:id>/', views.delete_order, name='delete_order'),
+
+    path('reset_password',
+         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+         name='reset_password'
+         ),
+    path('reset_password_sent',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'),
+         name='password_reset_done'
+         ),
+    path('reset_password/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset.html'),
+         name='password_reset_confirm'
+         ),
+    path('reset_password_complete',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_complete'
+         ),
 ]
